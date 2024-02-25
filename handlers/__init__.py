408ce3598abffef8db85eb554d5ea324823db1f5
MrGrmm <gramma1997ion@gmail.com>
2024-02-25 17:22:53 +0100

    update arhitecture handlers

---
+++ handlers/__init__.py
@@ -0,0 +1,8 @@
+from .command_start_handler import command_start_handler
+from .echo_handler import echo_handler
+
+# You can create a list called __all__ to define what symbols the package exports
+__all__ = [
+    'command_start_handler',
+    'echo_handler'
+]
